6f441e89638e9470ebabd9c95f40e502c5ec924f
kreid333 <kairetech@gmail.com>
2023-04-03 17:06:24 -0400

    added seeds file

---
+++ seeds/seeds.sql
@@ -0,0 +1,44 @@
+CREATE TABLE twentytwenty (
+id INT AUTO_INCREMENT NOT NULL,
+`rank` INT,
+company_name VARCHAR(100),
+number_of_employees INT,
+change_in_rank INT,
+revenues_in_millions INT,
+revenue_percent_change DECIMAL(7, 2),
+profits_in_millions INT,
+profit_percent_change DECIMAL(7, 2),
+assets_in_millions INT,
+market_value_in_millions INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE twentytwentyone (
+id INT AUTO_INCREMENT NOT NULL,
+`rank` INT,
+company_name VARCHAR(100),
+number_of_employees INT,
+change_in_rank INT,
+revenues_in_millions INT,
+revenue_percent_change DECIMAL(7, 2),
+profits_in_millions INT,
+profit_percent_change DECIMAL(7, 2),
+assets_in_millions INT,
+market_value_in_millions INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE twentytwentytwo (
+id INT AUTO_INCREMENT NOT NULL,
+`rank` INT,
+company_name VARCHAR(100),
+number_of_employees INT,
+change_in_rank INT,
+revenues_in_millions INT,
+revenue_percent_change DECIMAL(7, 2),
+profits_in_millions INT,
+profit_percent_change DECIMAL(7, 2),
+assets_in_millions INT,
+market_value_in_millions INT,
+PRIMARY KEY (id)
+);
